f6651bbe2760cbf68f0a2efd858d09d343448362
Mubangizi <mubangizia22@gmail.com>
2019-10-08 18:54:13 +0300

    initialising dp and app

--- app.py
+++ app.py
@@ -1,7 +1,36 @@
+import os
 from flask import Flask
+from flask_cors import CORS
 
-app = Flask(__name__)
+# import ORM
+from flask_sqlalchemy import SQLAlchemy
 
+
+# initialize sql-alchemy
+db = SQLAlchemy()
+
+def create_app(config_name):
+    """ app factory """
+    
+    # import config options
+    from config import app_config
+
+    app = Flask(__name__)
+
+    # allow cross-domain requests
+    CORS(app)
+
+    # use running config settings on app
+    app.config.from_object(app_config[config_name])
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # register app with the db
+    db.init_app(app)
+    
+    return app
+
+# create app instance using running config
+app = create_app(os.getenv('FLASK_ENV'))
 
 
 if __name__ == '__main__':
